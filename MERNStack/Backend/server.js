3c4169ffbca88b7cc349293193df97e5291dd293
Awadesh <36awadesh@gmail.com>
2024-07-29 19:40:42 +0530

    Create First API end point

--- MERNStack/Backend/server.js
+++ MERNStack/Backend/server.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// API end Point
+app.get("/", (req, res) => {
+    res.send("API is Running")
+})
+
+
 app.listen(5000, console.log("Server connected at 5000"));
