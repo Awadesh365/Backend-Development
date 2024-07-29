311652b57e87a0dbba6090295269cb648ef2f9c4
Awadesh <36awadesh@gmail.com>
2024-07-30 03:01:49 +0530

    LandingPae, Header, Footer

--- MERNStack/frontend/src/App.js
+++ MERNStack/frontend/src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
-const App = () => <h1>Hello world</h1>
+const App = () => {
+    return <>
+        <Header />
+        <main style={{ minHeight: "93vh" }}></main>
+        <Footer />
+    </>
+}
 
 export default App;
