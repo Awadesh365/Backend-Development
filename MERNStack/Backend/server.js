e403e9d060aed49158e296ce318a03b583b17409
Awadesh <36awadesh@gmail.com>
2024-07-29 20:19:04 +0530

    Create other api end points.

--- MERNStack/Backend/server.js
+++ MERNStack/Backend/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const notes = require('./data/notes.js');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 // API end Point
 app.get("/", (req, res) => {
     res.send("API is Running")
+});
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+    const curNote = notes.find((n) => n._id === req.params.id);
+    res.send(curNote);
 })
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log("Server connected at 5000"));
+app.listen(PORT, console.log(`Server connected at ${PORT}`));
