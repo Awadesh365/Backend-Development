d2759ae44956b2783eed82ed992892e9764fa5f8
Awadesh <36awadesh@gmail.com>
2024-08-05 03:16:06 +0530

    file upload by multer and store cloudinary

---
+++ Backend_Part_3/src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded sucessfully
+        console.log("file is uploaded on Cloudinary ", response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlink(localFilePath); // remove the locally saved tempory file
+        return null;
+    }
+}
