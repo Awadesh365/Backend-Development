4d034a66654466cf12a0621717501950b4b7cbba
Awadesh <36awadesh@gmail.com>
2024-08-10 18:26:30 +0530

    debug

--- Backend_Part_3/src/controllers/user.controller.js
+++ Backend_Part_3/src/controllers/user.controller.js
@@ -245,7 +245,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
-        .json(200, req.user, "Current user Fetched Successfully")
+        .json(new ApiResponse(200, req.user, "Current user Fetched Successfully"))
 })
 
 
