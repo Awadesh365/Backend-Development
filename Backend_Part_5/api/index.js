ccb63503e24aa2ebb9010e26f395ed54187bb8ab
Awadesh <36awadesh@gmail.com>
2024-08-18 19:19:48 +0530

    create sign-up api route

--- Backend_Part_5/api/index.js
+++ Backend_Part_5/api/index.js
@@ -1,26 +1,29 @@
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import express from "express"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
-import userRouter from "./routes/user.route.js"
-
-mongoose.connect(process.env.MONGO)
+mongoose
+    .connect(process.env.MONGO)
     .then(() => {
-        console.log("mongodb connected");
-
+        console.log('Connected to MongoDB!');
     })
     .catch((err) => {
-        console.log("not connected", err)
-    })
+        console.log(err);
+    });
 
 const app = express();
 
-app.use("/api/user", userRouter)
+app.use(express.json());
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
 
 app.listen(3000, () => {
-    console.log("server is running on Port 3000");
-})
+    console.log('Server is running on port 3000!');
+});
 
 
 
