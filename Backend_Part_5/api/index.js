2bce4055cf3a631f3aac30ac40cb5ce8d24495f5
Awadesh <36awadesh@gmail.com>
2024-08-18 15:35:52 +0530

    Connect to DB

--- Backend_Part_5/api/index.js
+++ Backend_Part_5/api/index.js
@@ -1,7 +1,30 @@
+import dotenv from "dotenv"
+import mongoose, { mongo } from "mongoose"
 import express from "express"
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("mongodb connected");
+
+    })
+    .catch((err) => {
+        console.log("not connected", err)
+    })
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("server is running on Port 3000");
 })
+
+
+// by using async await
+
+// const main = async () => {
+//     await mongoose.connect(process.env.MONGO);
+//     console.log("Monogdb connected through async/await");
+// }
+// main().catch((err) => {
+//     console.log("not connected through async awit", err);
+// })
