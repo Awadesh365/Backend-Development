7d71437ff964b6e544af30fb36408a880776b196
Awadesh <36awadesh@gmail.com>
2024-08-27 15:01:57 +0530

    Connect to Database [MongoDb]

--- auth/api/index.js
+++ auth/api/index.js
@@ -1,7 +1,33 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config();
+
+// connect DB using Promesis
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("MongoDB connected");
+    }).catch((err) => {
+        console.log("MongoDB Connection error", err);
+    })
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("server is running in port 3000");
 })
+
+
+// connect DB using async await 
+
+// const connectDb = async () => {
+//     try {
+//         const db = await mongoose.connect(process.env.MONGO)
+//         console.log("MongoDB connected");
+
+//     } catch (error) {
+//         console.log("MongoDB connection Error", error);
+//     }
+// }
+// connectDb()
