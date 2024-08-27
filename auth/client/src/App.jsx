7369983f9f67840aa6e0b0dcffbbe43c72445991
Awadesh <36awadesh@gmail.com>
2024-08-27 13:59:46 +0530

    Create header component and add Link from react router to attach the routes
    -  the addition of Link in the page is the second step of adding routes in react project.
    - this is done like :
     <Link to="/">
              <h1 className="font-bold">Auth</h1>
            </Link>

--- auth/client/src/App.jsx
+++ auth/client/src/App.jsx
@@ -5,10 +5,12 @@ import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
