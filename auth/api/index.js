b54440daec2dcdd2d5ecdfbdcdcf59c6d052c13c
Awadesh <36awadesh@gmail.com>
2024-08-27 21:29:46 +0530

    create test api route

--- auth/api/index.js
+++ auth/api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config();
+import userRouter from "./routes/user.route.js";
 
 // connect DB using Promesis
 mongoose
@@ -14,20 +15,8 @@ mongoose
 
 const app = express();
 
+app.use("/", userRouter)
+
 app.listen(3000, () => {
     console.log("server is running in port 3000");
 })
-
-
-// connect DB using async await 
-
-// const connectDb = async () => {
-//     try {
-//         const db = await mongoose.connect(process.env.MONGO)
-//         console.log("MongoDB connected");
-
-//     } catch (error) {
-//         console.log("MongoDB connection Error", error);
-//     }
-// }
-// connectDb()
