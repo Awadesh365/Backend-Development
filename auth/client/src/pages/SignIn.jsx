c90e9c38f2d731106d8b9de8575a3fd817a6473a
Awadesh <36awadesh@gmail.com>
2024-08-29 01:37:14 +0530

    Complete Signin page functionlaity
    - complete signin page functionality.
    - integrate the formData controller to signin (backend)
    - use of axios for simplicity and better
    - usenavigate to navigate to somewhere after everything woks fine (gnerally about lines written) [a react-router-dom hook]

--- auth/client/src/pages/SignIn.jsx
+++ auth/client/src/pages/SignIn.jsx
@@ -1,5 +1,63 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+
+      const res = await axios.post("/api/auth/signin", formData);
+
+      setLoading(false);
+      navigate("/"); // when about lines are good and everything works fine, then this will navigate to home page.
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+    </div>
+  );
 }
