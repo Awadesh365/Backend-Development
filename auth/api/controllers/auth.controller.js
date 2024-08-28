796dc1a705cb0106afd1e9710a245fd37ebf82a0
Awadesh <36awadesh@gmail.com>
2024-08-29 00:42:03 +0530

    add JWT authentication: 3 steps Game and create Signin API Route.
    // JWT Authentication: This process generates a token containing unique user identifiers, which is then stored in the browser's cookies.
    // The backend validates this token with each request, eliminating the need to re-enter email and password repeatedly.
    
    // MongoDB generates a unique _id for each user, which is commonly used to create the token.
    // The use of _id for token generation enhances security because it's less predictable compared to email or username, reducing the risk of unauthorized token creation.
    
    // The token is created using a combination of the user's _id and a secret key (JWT_SECRET).
    // If the JWT_SECRET changes, the token will also change, making old tokens invalid.
    
    // If no expiry date is specified, the token will only last for the session.
    // This means the user will have to log in again if they close the browser or the tab.

--- auth/api/controllers/auth.controller.js
+++ auth/api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -16,5 +17,32 @@ export const signUp = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
 
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404, "invalid credentials"))
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(404), "invalid credentials")
+
+        const { password: hashedPassword, ...rest } = validUser._doc; // this line will not let password go the client side.
+
+        // Jwt Auth: Token combination of unique identifiers of a user, and then we add it to the cookie of the browser.
+        // also when backend respond it checks the token each time and not ask for email and passwowrd each time.
+        // as _id created by mongodb will be always unique so it is used to create the token.
+        // token are created by _id in most of the cases due to security reasons, as with email and username there might be a case that someone will create a token.
+        // the token is combination of the _id and the  JWT_SECTRET you give, if you'll change the JWT_SECRET the tocken will be changed.
+        // if we do not add expiryDate, then the token will be for a session, which means when a user will close brwoser or tab he needs to lgoin again.
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // step 1
+        const expiryDate = new Date(Date.now() + 3600000); // 1 Hour
+        res.cookie("access_token", token, { httpOnly: true, expires: expiryDate }) // step 2
+            .status(200)
+            .json(validUser)
+    } catch (error) {
+        next(error)
+    }
 }
--- auth/api/routes/auth.route.js
+++ auth/api/routes/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express"
 import { signUp } from "../controllers/auth.controller.js"
+import { signIn } from "../controllers/auth.controller.js"
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+
+router.post("/signin", signIn)
 
 export default router;
